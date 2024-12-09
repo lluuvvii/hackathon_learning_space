2d12ec2014d120ac31c2a22c749e270bf82d8467
lluuvvii <kurniawanluvi64@gmail.com>
2024-12-09 21:56:37 +0700

    update dropdown profile(mobile view) & update projects page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,16 +13,171 @@ const links = [
   { name: 'Proyek', path: '/projects' },
   { name: 'Komunitas', path: '/community' },
   { name: 'Event', path: '/events' },
+  { name: 'Tentang Kami', path: '/about' }
 ]
+
+// const Navbar = () => {
+//   const pathname = usePathname()
+//   const shouldShowNavbar = !pathname.startsWith('/auth')
+
+//   const [dropdownVisible, setDropdownVisible] = useState(false)
+
+//   const toggleDropdown = () => {
+//     setDropdownVisible(!dropdownVisible)
+//   }
+
+//   return (
+//     <>
+//       {shouldShowNavbar ? (
+//         <>
+//           {/* Navbar */}
+//           <nav
+//             className='navbar navbar-expand-lg fixed-top'
+//             style={{ backgroundColor: '#00897B' }}
+//           >
+//             <div className='container'>
+//               {/* Logo */}
+//               <Link href='/' className='navbar-brand d-flex align-items-center gap-2 text-white'>
+//                 <Image
+//                   src={Logo}
+//                   alt='Hackathon Logo'
+//                   width={80}
+//                   height={40}
+//                   quality={100}
+//                 />
+//               </Link>
+
+//               {/* Toggler for mobile */}
+//               <button
+//                 className='navbar-toggler navbar-dark'
+//                 type='button'
+//                 data-bs-toggle='offcanvas'
+//                 data-bs-target='#offcanvasNavbar'
+//                 aria-controls='offcanvasNavbar'
+//                 aria-label='Toggle navigation'
+//               >
+//                 <span className='navbar-toggler-icon'></span>
+//               </button>
+
+//               {/* Navigation Links (desktop view) */}
+//               <div className='d-none d-lg-block'>
+//                 <ul className='navbar-nav ms-auto d-flex align-items-center'>
+//                   {links.map((link, index) => (
+//                     <li className='nav-item' key={index}>
+//                       <Link href={link.path} className='nav-link text-white'>
+//                         {link.name}
+//                       </Link>
+//                     </li>
+//                   ))}
+
+//                   {/* Profile Dropdown */}
+//                   <li className='nav-item dropdown ms-3 position-relative'>
+//                     <button
+//                       className='btn p-0 border-0 bg-transparent'
+//                       onClick={toggleDropdown}
+//                     >
+//                       <Image
+//                         src={ProfileUser}
+//                         alt='Profile'
+//                         width={40}
+//                         height={40}
+//                         className='rounded-circle'
+//                       />
+//                     </button>
+
+//                     {dropdownVisible && (
+//                       <ul
+//                         className='dropdown-menu show position-absolute'
+//                         style={{ top: '100%', right: 0 }}
+//                       >
+//                         <li>
+//                           <Link href='/profile' className='dropdown-item'>
+//                             Profil
+//                           </Link>
+//                         </li>
+//                         <li>
+//                           <Link href='/auth/login' className='dropdown-item'>
+//                             Masuk
+//                           </Link>
+//                         </li>
+//                         <li>
+//                           <button
+//                             className='dropdown-item'
+//                             onClick={() => alert('Logout clicked!')}
+//                           >
+//                             Keluar
+//                           </button>
+//                         </li>
+//                       </ul>
+//                     )}
+//                   </li>
+//                 </ul>
+//               </div>
+//             </div>
+//           </nav>
+
+//           {/* Offcanvas Sidebar (mobile view) */}
+//           <div
+//             className='offcanvas offcanvas-start'
+//             tabIndex={-1}
+//             id='offcanvasNavbar'
+//             aria-labelledby='offcanvasNavbarLabel'
+//           >
+//             <div className='offcanvas-header' style={{ backgroundColor: '#00897B' }}>
+//               <h5 className='offcanvas-title text-white' id='offcanvasNavbarLabel'>
+//                 Menu
+//               </h5>
+//               <button
+//                 type='button'
+//                 className='btn-close btn-close-white'
+//                 data-bs-dismiss='offcanvas'
+//                 aria-label='Close'
+//               ></button>
+//             </div>
+//             <div className='offcanvas-body'>
+//               {/* Offcanvas Links */}
+//               <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
+//                 {links.map((link, index) => (
+//                   <li className='nav-item' key={index}>
+//                     <Link href={link.path} className='nav-link text-dark'>
+//                       {link.name}
+//                     </Link>
+//                   </li>
+//                 ))}
+//                 <li><Link href='/auth/login' className='nav-link text-dark'>Masuk</Link></li>
+//               </ul>
+
+//               {/* Profile Icon in Offcanvas */}
+//               <div className='mt-3'>
+//                 <Image
+//                   src={ProfileUser}
+//                   alt='Profile'
+//                   width={40}
+//                   height={40}
+//                   className='rounded-circle'
+//                 />
+//               </div>
+//             </div>
+//           </div>
+//         </>
+//       ) : null}
+//     </>
+//   )
+// }
 
 const Navbar = () => {
   const pathname = usePathname()
   const shouldShowNavbar = !pathname.startsWith('/auth')
 
   const [dropdownVisible, setDropdownVisible] = useState(false)
+  const [offcanvasDropdownVisible, setOffcanvasDropdownVisible] = useState(false)
 
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible)
+  }
+
+  const toggleOffcanvasDropdown = () => {
+    setOffcanvasDropdownVisible(!offcanvasDropdownVisible)
   }
 
   return (
@@ -143,18 +298,48 @@ const Navbar = () => {
                     </Link>
                   </li>
                 ))}
-                <li><Link href='/auth/login' className='nav-link text-dark'>Masuk</Link></li>
               </ul>
 
               {/* Profile Icon in Offcanvas */}
               <div className='mt-3'>
-                <Image
-                  src={ProfileUser}
-                  alt='Profile'
-                  width={40}
-                  height={40}
-                  className='rounded-circle'
-                />
+                <button
+                  className='btn p-0 border-0 bg-transparent'
+                  onClick={toggleOffcanvasDropdown}
+                >
+                  <Image
+                    src={ProfileUser}
+                    alt='Profile'
+                    width={40}
+                    height={40}
+                    className='rounded-circle'
+                  />
+                </button>
+
+                {offcanvasDropdownVisible && (
+                  <ul
+                    className='dropdown-menu show position-absolute mt-2'
+                    style={{ left: '50%', transform: 'translateX(-50%)' }}
+                  >
+                    <li>
+                      <Link href='/profile' className='dropdown-item'>
+                        Profil
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href='/auth/login' className='dropdown-item'>
+                        Masuk
+                      </Link>
+                    </li>
+                    <li>
+                      <button
+                        className='dropdown-item'
+                        onClick={() => alert('Logout clicked!')}
+                      >
+                        Keluar
+                      </button>
+                    </li>
+                  </ul>
+                )}
               </div>
             </div>
           </div>
